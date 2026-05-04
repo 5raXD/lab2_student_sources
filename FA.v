72f9c47c298747e0ba3e20118ed61bb94704a169
mahmood stitia <mahmoodi@mail.tau.ac.il>
2026-05-04 12:51:47 +0300

    add csa

---
+++ FA.v
@@ -0,0 +1,33 @@
+`timescale 1ns/10ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company:         Tel Aviv University
+// Engineer:         Saleh Khalil , mahmood stitia 
+// 
+// Create Date:     11/12/2018 08:59:38 PM
+// Design Name:     EE3 lab1
+// Module Name:     FA
+// Project Name:    Electrical Lab 3, FPGA Experiment #1
+// Target Devices:  Xilinx BASYS3 Board, FPGA model XC7A35T-lcpg236C
+// Tool Versions:   Vivado 2016.4
+// Description:     Well known full adder
+// 
+// Dependencies:    None
+// 
+// Revision:        2.0
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+module FA(a, b, ci, sum, co);
+
+  input   a, b, ci;
+  output  sum, co;
+  wire axb;
+  
+  // FILL HERE
+  assign axb = a^b; // a xor b
+  assign sum = axb^ci; // a^b^ci
+  //calculate carry out
+  assign co = (a&b) | (axb)&ci ;
+  
+
+endmodule
